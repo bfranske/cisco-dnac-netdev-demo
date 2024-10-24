07f1e0dc26e983db7e80769f44df839558f36664
Ben Franske <ben.mm@franske.com>
2024-10-23 19:42:22 -0500

    fix print_interface_info column alignment

--- get_device_interfaces.py
+++ get_device_interfaces.py
@@ -32,10 +32,10 @@ def get_auth_token():
     return token    # Create a return statement for the Token
 
 def print_interface_info(interface_info):
-    print("{0:42}{1:17}{2:12}{3:18}{4:17}{5:10}{6:15}".
+    print("{0:30}{1:10}{2:12}{3:18}{4:17}{5:10}{6:15}".
           format("portName", "vlanId", "portMode", "portType", "duplex", "status", "lastUpdated"))
     for int in interface_info['response']:
-        print("{0:42}{1:10}{2:12}{3:18}{4:17}{5:10}{6:15}".
+        print("{0:30}{1:10}{2:12}{3:18}{4:17}{5:10}{6:15}".
               format(str(int['portName']),
                      str(int['vlanId']),
                      str(int['portMode']),
